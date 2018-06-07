cf346c5626afdcc328d4faf5bbafb6ce2d6ddd18
fy <420585251@qq.com>
2018-06-07 23:10:14 +0800

    merge files

--- gitlab.sh
+++ gitlab.sh
@@ -1,24 +1,32 @@
 #!/usr/bin/bash
 clear
-#安装依赖包
+# install dependencies
 sudo yum install curl policycoreutils openssh-server openssh-clients
 sudo systemctl enable sshd
 sudo systemctl start sshd
+
+sudo firewall-cmd --permanent --add-service=http
+sudo systemctl reload firewalld
+
+# install Postfix
 sudo yum install postfix
 sudo systemctl enable postfix
 sudo systemctl start postfix
-#下载包
-curl -O https://mirrors.tuna.tsinghua.edu.cn/gitlab-ce/yum/el7/gitlab-ce-8.8.3-ce.0.el7.x86_64.rpm
-rpm -i gitlab-ce-8.8.3-ce.0.el7.x86_64.rpm
-gitlab-ctl reconfigure
-gitlab-ctl start
 
-#https://github.com/larryli/gitlabhq/
-#下载
-curl -O http://192.168.100.83/packet/gitlab-rails.tar
-tar -xvf gitlab-rails.tar
-gitlab-ctl stop
-cp -r /opt/gitlab/embedded/service/gitlab-rails /opt/gitlab/embedded/service/gitlab-rails-bakup
-cp -fr gitlab-rails /opt/gitlab/embedded/service/gitlab-rails
-gitlab-ctl start
-gitlab-ctl reconfigure
+# Add the GitLab package repository 
+#curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh | sudo bash
+
+# install the package
+#sudo yum install gitlab-ce
+
+# rpm install package
+cat /etc/redhat-release
+# 
+sudo curl -O https://mirrors.tuna.tsinghua.edu.cn/gitlab-ce/yum/el7/gitlab-ce-10.3.5-ce.0.el7.x86_64.rpm
+# 
+sudo rpm -ivh gitlab-ce-10.3.5-ce.0.el7.x86_64.rpm
+
+# 3. 启动GitLab
+sudo gitlab-ctl reconfigure
+
+sudo gitlab-ctl start
--- mysql/mariadb-galera.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-clear
-echo "Install mariadb-galera"
-curl -O https://mirrors.tuna.tsinghua.edu.cn/mariadb//mariadb-galera-5.5.49/source/mariadb-galera-5.5.49.tar.gz
-tar zxvf mariadb-galera-5.5.49.tar.gz
-echo "##################################################################"
