1a1477c87098e3540e22fd26aa353b471b453d4e
YF18 <success256@hotmail.com>
2018-09-06 23:55:02 +0800

    Create monitor.sh

---
+++ monitor.sh
@@ -0,0 +1,23 @@
+https://www.jianshu.com/p/378d0005c0a4
+
+界面
+  Grafana
+  
+收集
+  Telegraf
+  
+存储
+  Influxdb
+
+工具
+  wrk 
+
+https://www.cnblogs.com/ycyzharry/p/8372168.html
+
+参考官方文档
+
+    telegraf: https://docs.influxdata.com/telegraf/v1.0/
+    influxdb: https://docs.influxdata.com/influxdb/v1.0
+    grafana: http://docs.grafana.org/
+https://blog.csdn.net/li_xue_zhao/article/details/79800140
+https://blog.csdn.net/w958660278/article/details/80484486
