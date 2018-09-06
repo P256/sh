8f2d734797d39538d3517c373edcf358da952bb8
YF18 <success256@hotmail.com>
2018-09-07 00:52:19 +0800

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,4 +1,3 @@
-https://www.jianshu.com/p/378d0005c0a4
 
 界面
   Grafana
@@ -12,12 +11,31 @@ https://www.jianshu.com/p/378d0005c0a4
 工具
   wrk 
 
-https://www.cnblogs.com/ycyzharry/p/8372168.html
+
 
 参考官方文档
 
     telegraf: https://docs.influxdata.com/telegraf/v1.0/
     influxdb: https://docs.influxdata.com/influxdb/v1.0
     grafana: http://docs.grafana.org/
+    
+    
 https://blog.csdn.net/li_xue_zhao/article/details/79800140
 https://blog.csdn.net/w958660278/article/details/80484486
+https://www.jianshu.com/p/378d0005c0a4
+https://www.jianshu.com/p/dfd329d30891
+https://blog.csdn.net/liurui_wuhan/article/details/79001756
+https://blog.csdn.net/liurui_wuhan/article/details/79001756
+https://www.cnblogs.com/ycyzharry/p/8372168.html
+
+
+
+
+
+
+
+
+
+
+
+
