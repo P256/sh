b6d259c156f00d04869e1ae56ffd10de19d8dc88
YF18 <success256@hotmail.com>
2018-09-10 17:34:58 +0800

    Update logstash.sh

--- logstash.sh
+++ logstash.sh
@@ -3,3 +3,9 @@ curl -O　https://artifacts.elastic.co/downloads/logstash/logstash-6.4.0.tar.gz
 #
 tar zxvf logstash-6.4.0.tar.gz
 #
+mv logstash-6.4.0 ../logstash
+#
+../logstash/bin/logstash -f logstash.conf
+
+
+
