88bc51f32b76505c0d84c370339d0172c9a11d03
YF18 <success256@hotmail.com>
2018-09-10 17:28:35 +0800

    Update kibana.sh

--- kibana.sh
+++ kibana.sh
@@ -1,5 +1,20 @@
 #
 curl -O https://artifacts.elastic.co/downloads/kibana/kibana-6.4.0-linux-x86_64.tar.gz
 #
-
+tar zxvf kibana-6.4.0-linux-x86_64.tar.gz
 #
+mv kibana-6.4.0-linux-x86_64 /usr/local/kibana
+# 
+vi /usr/local/kibana/config/kibana.yml
+# 
+#server.host "192.168.1.62"
+#server.name: "kibana"
+#elasticsearch.url "http://192.168.1.62:9200"
+
+
+##################################################################################################
+# plugin
+##################################################################################################
+# list
+kibana/bin/kibana-plugin list
+
