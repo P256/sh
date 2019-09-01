3ca9816c9b5214c2339a6f36f7b8cc5822f07b19
Alex <SUC168@qq.com>
2019-09-01 15:14:52 +0800

    edit files

--- tick/grafana.sh
+++ tick/grafana.sh
@@ -1,11 +1,15 @@
 ####################################################################################################
+
+version=grafana-5.2.4
 # 二进制版本
-curl -O https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana-5.2.4.linux-amd64.tar.gz
+curl -O https://dl.grafana.com/oss/release/${version}.linux-amd64.tar.gz
 #
-tar zxvf grafana-5.2.4.linux-amd64.tar.gz
+tar zxvf ${version}.linux-amd64.tar.gz
 #
 mv grafana-5.2.4 ../grafana
 #
 cd ../grafana
 #
 
+curl -O https://dl.grafana.com/oss/release/grafana-6.3.4.linux-amd64.tar.gz 
+tar zxvf grafana-6.3.4.linux-amd64.tar.gz 
